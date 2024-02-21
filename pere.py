336328285ec52cca61d347c7e239220c0175c0bb
Manuel Soto <158333386+ManuelSotoOrtiz@users.noreply.github.com>
2024-02-21 19:41:19 +0100

    New changes

--- pere.py
+++ pere.py
@@ -1,3 +1,5 @@
+from turtle import *
+import colorsys
 def AitorFunc():
     print("Aitor")
 
@@ -10,27 +12,23 @@ def VictorFunc():
     print("Victor")
 
 
-def ManuFunc():
-    print("Manu")  
-from turtle import *
-import colorsys
+def ManuFunc(): 
+    speed(3000)
+    bgcolor('black')
+    h = 0
 
-speed(3000)
-bgcolor('black')
-h = 0
-
-for i in range(16):
-    for j in range(18):
-        c = colorsys.hsv_to_rgb(h,0.9,1)
-        color(c)
-        h += 0.005
-        rt(90)
-        circle(150-j*6,90)
-        lt(90)
-        circle(150-j*6,90)
-        rt(180)
-    circle (40,24)
-done()
+    for i in range(16):
+        for j in range(18):
+            c = colorsys.hsv_to_rgb(h,0.9,1)
+            color(c)
+            h += 0.005
+            rt(90)
+            circle(150-j*6,90)
+            lt(90)
+            circle(150-j*6,90)
+            rt(180)
+        circle (40,24)
+    done()
 
 AitorFunc()
 AngelFunc()
